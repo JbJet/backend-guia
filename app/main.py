0fc299d0d62c9f2d09157965fac643ddd63ba842
JbJet <jbgjetoficial@gmail.com>
2026-06-06 16:37:25 -0300

    Fix: cors

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["guia.joaquimb.xyz"],
+    allow_origins=["https://guia.joaquimb.xyz"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
